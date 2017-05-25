8285130937a2edfe36cc427fc67c54572d51c773
Mehmet Top <mehmet.dummydata@gmail.com>
2017-05-25 15:18:49 +0000

    DHT11 Implementation

--- Project/index.py
+++ Project/index.py
@@ -11,6 +11,18 @@
 from flask import Flask
 from flask import render_template
 
+import RPi.GPIO as GPIO
+import dht11
+import time
+
+# Initialize RPi GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# Map GPIO Pin 14 to DHT11 Sensor
+instance = dht11.DHT11(pin=14)
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -27,7 +39,19 @@ def page_information():
 
 @app.route('/dashboard')
 def page_dashboard():
-    return render_template('dashboard.html', temperature = 26.4,  humidity = 40.2)
+
+    temperature = "Error!"
+    humidity    = "Error!"
+
+    result = instance.read()
+    
+    while not result.is_valid():
+        result = instance.read()
+
+    temperature = result.temperature
+    humidity = result.humidity
+
+    return render_template('dashboard.html', temperature = temperature,  humidity = humidity)
 
 if __name__ == '__main__':
     app.run()
