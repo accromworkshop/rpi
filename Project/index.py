e714b5fad99287770246b4db50f3fb7b242fa53e
Mehmet Top <mehmet.dummydata@gmail.com>
2017-05-31 17:01:16 +0000

    Port and Style were added

--- Project/index.py
+++ Project/index.py
@@ -6,7 +6,7 @@
 #
 # Created:     23.05.2017
 # Copyright:   (c) mehmet.top 2017
-# Licence:     The Beer Licence
+# Licence:     Beer License
 #-------------------------------------------------------------------------------
 from flask import Flask
 from flask import request
@@ -14,32 +14,30 @@ from flask import render_template
 
 import RPi.GPIO as GPIO
 import dht11
-import time
+
+APP_HOST = "localhost"
+APP_PORT = 5001
+
+GPIO_PIN_LED = 18
+GPIO_PIN_SENSOR = 14
 
 # Initialize RPi GPIO
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-GPIO_PIN_LED = int(18)
-GPIO.setup(18, GPIO.OUT)
+
+
+GPIO.setup(GPIO_PIN_LED, GPIO.OUT)
 
 # Map GPIO Pin 14 to DHT11 Sensor
-instance = dht11.DHT11(pin=14)
+instance = dht11.DHT11(pin=GPIO_PIN_SENSOR)
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-
-@app.route('/main')
-def page_main():
-    return '<html><body><div>Welcome to Main Page</div></body></html>'
-
-@app.route('/information')
-def page_information():
-    return render_template('information.html')
 
 @app.route('/dashboard')
 def page_dashboard():
@@ -65,7 +63,7 @@ def page_dashboard():
     return render_template('dashboard.html', temperature = temperature,  humidity = humidity, ledStatus = ledStatus)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=APP_HOST, port=APP_PORT)
 
 
 
